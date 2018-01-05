a01cbb9126848b0440bccdfdcb8d7b97039ba6d2
Mahesh Daram <mahesh.daram@ggktech.com>
2018-01-05 15:15:47 +0530

    api changes

---
+++ WebAPI/Controllers/BaseController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Unity;
+using DataServices.Interfaces;
+using DataServices;
+
+namespace WebAPI.Controllers
+{
+
+    public class BaseController : Controller
+    {
+        protected IUnityContainer myContainer = new UnityContainer();
+
+        public BaseController()
+        {
+            RegisterTypes();
+        }
+
+        public void RegisterTypes()
+        {
+            myContainer.RegisterType(typeof(IRepository), typeof(OptomateRepository), "Optomate");
+            myContainer.RegisterType(typeof(IRepository), typeof(OptomateTouchRepository), "OptomateTouch");
+        }
+    }
+}
--- WebAPI/Controllers/HomeController.cs
+++ WebAPI/Controllers/HomeController.cs
@@ -7,39 +7,71 @@ using Microsoft.AspNetCore.Mvc;
 using BusinessServices;
 using DataServices;
 using BusinessServices.Interfaces;
+using DataServices.Interfaces;
+using Unity;
+using Newtonsoft;
+using Newtonsoft.Json;
+using Unity.Resolution;
+using Microsoft.Extensions.Configuration;
 
 namespace WebAPI.Controllers
 {
     [Produces("application/json")]
     [Route("api/Home")]
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         IPatientService _patientService;
-        public HomeController(IPatientService patientService)
+        IConfiguration _configuration;
+        string _connectionString = string.Empty;
+
+        public HomeController(IConfiguration configuration)
         {
-            _patientService = patientService;
+            _configuration = configuration;
         }
 
         // GET: api/Home
         [HttpGet]
-        public IEnumerable<string> GetPatients(string PMS)
+        public string GetPatients(string PMS)
         {
-            //ToDo: Need to figure out best way for resolving this
-            //if (PMS == "Optomate")
-            //{
-            //    _patientService = new PatientService(new OptomateRepository(""));
-            //}
-            //else if (PMS == "OptomateTouch")
-            //{
-            //    _patientService = new PatientService(new OptomateTouchRepository(""));
-            //}
 
-            _patientService.GetPatients();
+            if (!string.IsNullOrEmpty(PMS))
+            {
+                _connectionString = _configuration.GetValue<string>("ConnectionString :" + PMS);
+                _patientService = new PatientService(myContainer.Resolve<IRepository>(PMS, new ResolverOverride[]
+                                  {
+                                       new ParameterOverride("ConnectionString",_connectionString )
+                                  }));
 
-            return new string[] { "value1", "value2" };
+                var patients = _patientService.GetPatients();
+
+                return JsonConvert.SerializeObject(patients);
+            }
+
+            return string.Empty;
+
         }
 
-        // GET: api/Home/5
+        [HttpGet]
+        public string GetPatient(string PMS, int Id)
+        {
+
+            if (!string.IsNullOrEmpty(PMS))
+            {
+                _connectionString = _configuration.GetValue<string>("ConnectionString :" + PMS);
+                _patientService = new PatientService(myContainer.Resolve<IRepository>(PMS, new ResolverOverride[]
+                                  {
+                                       new ParameterOverride("ConnectionString", _connectionString)
+                                  }));
+
+                var patient = _patientService.GetPatient(Id);
+
+                return JsonConvert.SerializeObject(patient);
+            }
+
+            return string.Empty;
+
+        }
+
 
     }
 }
